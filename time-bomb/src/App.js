8b5f0106628e7be26d2f4213a9397876bbb629f9
dieeel <by.gonece@gmail.com>
2023-11-05 22:49:10 +0900

    player table

--- time-bomb/src/App.js
+++ time-bomb/src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
 
-function App() {
+function PlayerRow({ player }) {
   return (
-    <div>
-      hello world.
-    </div>
+    <tr>
+      <td>{player.name}</td>
+      <td>{player.job}</td>
+    </tr>
   );
 }
 
-export default App;
+function PlayerTalbe({ players }) {
+  const rows = [];
+
+  players.forEach((player) => {
+    rows.push(
+      <PlayerRow player={player} key={player.name} />
+    )
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>名前</th>
+          <th>役割</th> </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+const PLAYERS = [
+  {name: "のびた", job: "警察"},
+  {name: "ジャイアン", job: "警察"},
+  {name: "しずかちゃん", job: "警察"},
+  {name: "スネ夫", job: "警察"},
+];
+
+export default function App() {
+  return <PlayerTalbe players={PLAYERS}/>;
+}
