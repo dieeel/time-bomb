38c3b231d522f1114cc82e182db0561a82eaf413
dieeel <by.gonece@gmail.com>
2023-11-08 22:59:24 +0900

    create user table component.

--- time-bomb/src/App.js
+++ time-bomb/src/App.js
@@ -1,42 +1,5 @@
-import './App.css';
-
-function PlayerRow({ player }) {
-  return (
-    <tr>
-      <td>{player.name}</td>
-      <td>{player.job}</td>
-    </tr>
-  );
-}
-
-function PlayerTalbe({ players }) {
-  const rows = [];
-
-  players.forEach((player) => {
-    rows.push(
-      <PlayerRow player={player} key={player.name} />
-    )
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>名前</th>
-          <th>役割</th> </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-const PLAYERS = [
-  {name: "のびた", job: "警察"},
-  {name: "ジャイアン", job: "警察"},
-  {name: "しずかちゃん", job: "警察"},
-  {name: "スネ夫", job: "警察"},
-];
+import UserTable from './UserTable';
 
 export default function App() {
-  return <PlayerTalbe players={PLAYERS}/>;
+  return <UserTable />;
 }
